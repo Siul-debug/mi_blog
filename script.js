3c663d565100a552f096765ad6bcb8800c4ca31f
Luis Miguel Remuzgo Pita <1923517@alu.murciaeduca.es>
2026-02-11 12:27:03 +0100

    Etapa 5: personalización del script

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Mensaje que aparece en la consola cuando se carga la página
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Página cargada correctamente.');
+  console.log('¡Bienvenido a mi blog, Luis!');
+  
+  // Seleccionamos el encabezado <h1>
+  const titulo = document.querySelector('header h1');
+  
+  // Añadimos un evento al hacer clic en el título
+  titulo.addEventListener('click', () => {
+    alert('¡Gracias por visitar mi blog!');
+  });
 });
